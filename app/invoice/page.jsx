442917cec43a187610f0cc3c511ef089c2e3af12
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-06-07 21:32:27 +0600

    Invoice call

---
+++ app/invoice/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Invoice from "../components/Invoice";
+
+const page = () => {
+  return (
+    <div className="px-24 border-spacing-2 border-indigo-500">
+      <Invoice />
+    </div>
+  );
+};
+
+export default page;
