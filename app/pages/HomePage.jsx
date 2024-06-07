fffc61c325b33acdc7f33e64a33e1c79dc4abf72
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-06-07 14:05:58 +0600

    Add HomePage where all the compo will be loaded

---
+++ app/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReservationForm from "../components/ReservationForm";
+
+const HomePage = () => {
+  return (
+    <div className="">
+      <ReservationForm />
+    </div>
+  );
+};
+
+export default HomePage;
